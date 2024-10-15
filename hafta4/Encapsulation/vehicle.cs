5e85256d87776066281ace0ea73628d51c03c442
begüm doğanay <86783970+begumdoganay@users.noreply.github.com>
2024-10-15 15:08:47 +0300

    added

---
+++ hafta4/Encapsulation/vehicle.cs
@@ -0,0 +1,13 @@
+﻿using _04Encapsulation;
+
+// Create a new Car object
+Car secondCar = new Car("BMW", "M5", "Blue", 3);
+
+// Display initial car info (expected error due to invalid door count)
+secondCar.CarInfo();
+
+// Correct the number of doors
+secondCar.NumberOfDoors = 4;
+
+// Display updated car info
+secondCar.CarInfo();
